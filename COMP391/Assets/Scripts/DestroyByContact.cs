a0bd612810d129e2ba4d2109fc59a3f30e463ee4
RizwanSamji <36008740+RizwanSamji@users.noreply.github.com>
2018-03-07 14:12:33 -0500

    [ADD] Asteroid and Explosion

---
+++ COMP391/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+    public GameObject explosionAsteroid;
+    // Use this for initialization
+    void Start()
+    {
+    }
+        void OnTriggerEnter2D(Collider2D other)
+        {
+        if(other.tag == "Boundary")
+        {
+            return;
+        }
+
+        Instantiate(explosionAsteroid, this.transform.position, this.transform.rotation);
+
+        Destroy(other.gameObject);
+        Destroy(this.gameObject);
+    }
+	
+}
