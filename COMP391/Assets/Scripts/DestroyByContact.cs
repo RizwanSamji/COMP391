1073ecd0f3f8dc4f999afec0b0b3ca60b2bfc4ba
RizwanSamji <36008740+RizwanSamji@users.noreply.github.com>
2018-03-14 14:19:14 -0400

    [ADDED] asteroid waves + game over

--- COMP391/Assets/Scripts/DestroyByContact.cs
+++ COMP391/Assets/Scripts/DestroyByContact.cs
@@ -5,15 +5,34 @@ using UnityEngine;
 public class DestroyByContact : MonoBehaviour {
 
     public GameObject explosionAsteroid;
+    public GameObject explosionPlayer;
+
+    private GameController gameControllerScript;
+
     // Use this for initialization
     void Start()
     {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if(gameControllerObject != null)
+        {
+            gameControllerScript = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameControllerScript == null)
+        {
+            Debug.Log("Cannot find Game Controller script on Object");
+        }
     }
         void OnTriggerEnter2D(Collider2D other)
         {
-        if(other.tag == "Boundary")
+        if(other.tag == "Boundary"||other.tag == "Hazard")
         {
             return;
+        }
+
+        if(other.tag == "Player")
+        {
+            Instantiate(explosionPlayer, other.transform.position, other.transform.rotation);
+            gameControllerScript.GameOver();
         }
 
         Instantiate(explosionAsteroid, this.transform.position, this.transform.rotation);
---
+++ COMP391/Assets/Scripts/GameController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard;
+    public Vector2 spawnValue;
+    public int hazardCount;
+    public float startWait;
+    public float spawnWait;
+    public float waveWait;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        StartCoroutine(SpawnWaves());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait); //pause
+        while(true)
+        {
+            for(int i = 0; i < hazardCount; i++)
+            {
+                Vector2 spawnPosition = new Vector2(spawnValue.x, Random.Range(-spawnValue.y,spawnValue.y));
+                Quaternion spawnRotation = Quaternion.identity; //default rotation
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+
+            }
+            yield return new WaitForSeconds(waveWait);
+            if(gameOver)
+            {
+                break;
+            }
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        Debug.Log("Score is " + score);
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("Game is Over");
+        gameOver = true;
+    }
+}
